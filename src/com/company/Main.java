bbbfe1a05e1f903be9e7efb1301fb9ba86a8d696
java01 <sergo231993@gmail.com>
2020-09-01 19:44:08 +0300

    часть 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,17 +3,42 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	int chislo = 123;
-	String chislo2 = "123";
-	String result = chislo2 + 1;
-        System.out.println(result);
-        int chislo3 = Integer.valueOf(chislo2);
-        System.out.println(chislo3 + 2);
+        String harryPotter = "Mr. and Mrs. Dursley, of number four, Privet Drive, were proud to say that they were " +
+                "perfectly normal, thank you very much. They were the last people you'd expect to be involved in " +
+                "anything strange or mysterious, because they just didn't hold with such nonsense. \n" +
+                "\n" +
+                "Mr. Dursley was the director of a firm called Grunnings, which made drills. He was a big, beefy " +
+                "man with hardly any neck, although he did have a very large mustache. Mrs. Dursley was thin and " +
+                "blonde and had nearly twice the usual amount of neck, which came in very useful as she spent so " +
+                "much of her time craning over garden fences, spying on the neighbors. The Dursleys had a small son" +
+                " called Dudley and in their opinion there was no finer boy anywhere. \n" +
+                "\n" +
+                "The Dursleys had everything they wanted, but they also had a secret, and their greatest fear was" +
+                " that somebody would discover it. They didn't think they could bear it if anyone found out about" +
+                " the Potters. Mrs. Potter was Mrs. Dursley's sister, but they hadn't met for several years; in fact," +
+                " Mrs. Dursley pretended she didn't have a sister, because her sister and her good-for-nothing husband " +
+                "were as unDursleyish as it was possible to be. The Dursleys shuddered to think what the neighbors " +
+                "would say if the Potters arrived in the street. The Dursleys knew that the Potters had a small son," +
+                " too, but they had never even seen him. This boy was another good reason for keeping the Potters " +
+                "away; they didn't want Dudley mixing with a child like that. \n" +
+                "\n" +
+                "When Mr. and Mrs. Dursley woke up on the dull, gray Tuesday our story starts, there was nothing " +
+                "about the cloudy sky outside to suggest that strange and mysterious things would soon be happening " +
+                "all over the country. Mr. Dursley hummed as he picked out his most boring tie for work, and Mrs. " +
+                "Dursley gossiped away happily as she wrestled a screaming Dudley into his high chair. \n" +
+                "\n" +
+                "None of them noticed a large, tawny owl flutter past the window. ";
 
-        int summa = 0;
-        for (int i = 0; i < chislo2.length() ; i++) {
-            summa = summa + Integer.valueOf(chislo2.charAt(i));
+           String buffer = harryPotter.replaceAll("[^A-Z,a-z ]", "");
+        System.out.println(buffer);
+        String[] array = buffer.split(" ");
+        System.out.println(array[5]);
+        System.out.println(array.length);
+        for (int i = 0; i <array.length ; i++) {
+            if (array[i].length() == 4 && array[i].charAt(0) == 'a' || (array[i].length() == 7 && array[i].charAt(0) == 'a') ){
+                System.out.println(array[i]);
+            }
+
         }
-        System.out.println(summa);
     }
 }
